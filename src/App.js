6d57c3640eea03e2e0a4b08eda4d66eb4030344b
Uwe Kremer <uwekremer1971@gmx.de>
2022-10-19 15:22:42 +0200

    abgebrochener Versuch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [books, setBooks] = useState([])
+
+  useEffect(() => 
+  {
+    const fetchBooks = async () =>
+    {
+      const response =await fetch('https://anapioficeandfire.com/api/books');
+      const data = await response.json();
+      setBooks(data);
+    }
+    fetchBooks();
+  }, []);
+
+  return <div className="App">
+    <h1>Game of Thrones Books</h1>
+    <ul>
+      {books.map(book => (
+        <li>
+          <h3>{book.name}</h3>
+          <p>Jahr: {book.released}</p>
+        </li>
+      ))}
+    </ul>
+    </div>;
 }
 
 export default App;
