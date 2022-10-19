634e7d34c6743850ad6edb16a3299f0d299c4ef9
X-Ella-X <e.felsinger@web.de>
2022-10-19 15:21:25 +0200

    version 0.01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Area } from "./components/Area";
+import { Category } from "./components/Category";
+import { Footer } from "./components/Footer";
+import { Fridge } from "./components/Fridge";
+import { Header } from "./components/Header";
+import { Info } from "./components/Info";
+import { Meal } from "./components/Meal";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Meal />
+      <Area />
+      <Category />
+      <Fridge />
+      <Info />
+      <Footer />
     </div>
   );
 }
