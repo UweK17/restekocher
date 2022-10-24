2e2bd1028234bcc67319886ba5c3bc7cf3e66087
X-Ella-X <e.felsinger@web.de>
2022-10-24 15:48:38 +0200

    funktionierende icons 1/2

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,78 +7,82 @@ import {
   GiCheeseWedge,
 } from "react-icons/gi";
 import { IoLeaf } from "react-icons/io5";
-import styled from "styled-components";
+// import styled from "styled-components";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../category.css";
 
 export const Category = () => {
   return (
-    <List>
-      <SLink to={"/cuisine/Breakfast"}>
+    <div className="icons">
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+        to={"/cuisine/Breakfast"}
+      >
         <GiCroissant />
         <h4>Breakfast</h4>
-      </SLink>
-      <SLink to={"/cuisine/Beef"}>
+      </NavLink>
+      <NavLink className="icon" to={"/cuisine/Beef"}>
         <TbMeat />
         <h4>Beef</h4>
-      </SLink>
-      <SLink to={"/cuisine/Chicken"}>
+      </NavLink>
+      <NavLink className="icon" to={"/cuisine/Chicken"}>
         <GiChickenOven />
         <h4>Chicken</h4>
-      </SLink>
-      <SLink to={"/cuisine/Seafood"}>
+      </NavLink>
+      <NavLink className="icon" to={"/cuisine/Seafood"}>
         <GiShrimp />
         <h4>Seafood</h4>
-      </SLink>
-      <SLink to={"/cuisine/Vegetarian"}>
+      </NavLink>
+      <NavLink className="icon" to={"/cuisine/Vegetarian"}>
         <GiCheeseWedge />
         <h4>Vegetarian</h4>
-      </SLink>
-      <SLink to={"/cuisine/Vegan"}>
+      </NavLink>
+      <NavLink className="icon" to={"/cuisine/Vegan"}>
         <IoLeaf />
         <h4>Vegan</h4>
-      </SLink>
-      <SLink to={"/cuisine/Dessert"}>
+      </NavLink>
+      <NavLink className="icon" to={"/cuisine/Dessert"}>
         <GiCupcake />
         <h4>Dessert</h4>
-      </SLink>
-    </List>
+      </NavLink>
+    </div>
   );
 };
 
-const List = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 2rem 0rem;
-`;
-const SLink = styled(NavLink)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  jusitfy-content: center;
-  border-radius: 50%;
-  margin-right: 1rem;
-  text-decoration: none;
-  background: linear-gradient(35deg, #494949, #313131);
-  width: 4.7rem;
-  height: 4.7rem;
-  cursor: pointer;
+// const List = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   margin: 2rem 0rem;
+// `;
+// const SLink = styled(NavLink)`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   border-radius: 50%;
+//   margin-right: 1rem;
+//   text-decoration: none;
+//   background: linear-gradient(35deg, #494949, #313131);
+//   width: 4.7rem;
+//   height: 4.7rem;
+//   cursor: pointer;
 
-  h4 {
-    color: white;
-    font-size: 0.9rem;
-  }
-  svg {
-    color: white;
-    font-size: 2rem;
-  }
-  &.active {
-    background: linear-gradient(to right, #f27121, #e94057);
-    svg {
-      color: white;
-    }
-    h4 {
-      color: white;
-    }
-  }
-`;
+//   h4 {
+//     color: white;
+//     font-size: 0.8rem;
+//   }
+//   svg {
+//     color: white;
+//     font-size: 2rem;
+//   }
+//   &.active {
+//     background: linear-gradient(to right, #f27121, #e94057);
+//     svg {
+//       color: white;
+//     }
+//     h4 {
+//       color: white;
+//     }
+//   }
+// `;
