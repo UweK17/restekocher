73be664cd77140543b8bd8f0fa96896a29e42b13
X-Ella-X <e.felsinger@web.de>
2022-10-24 23:26:28 +0200

    add white-space to css & loop through measures + ingredients

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useParams, useNavigate } from "react-router-dom";
-import './recipe.css';
+import "./recipe.css";
 
 import React from "react";
 
@@ -21,82 +21,61 @@ export const Recipe = () => {
   }, [params.name]);
 
   const navigate = useNavigate();
-  const handleClick = () => 
-  {
-    navigate('/', {replace: true});
+  const handleClick = () => {
+    navigate("/", { replace: true });
   };
-//________LOOOOOP________________________________________________________________
- 
-// if(details.meals[0].strIngredient1 !== "")
-//   {
-//     const ing1= details.meals[0].strIngredient1;
-//     console.log(ing1);
-//     return ing1;
-//   }
+  //________LOOOOOP__________________________________________1______________________
 
-//________LOOOOOP________________________________________________________________
+  // if(details.meals[0].strIngredient1 !== "")
+  //   {
+  //     const ing1= details.meals[0].strIngredient1;
+  //     console.log(ing1);
+  //     return ing1;
+  //   }
 
+  //________LOOOOOP__________________________________________1______________________
+
+  //________LOOOOOP__________________________________________2______________________
+
+  // let ingredientsArray = [];
+
+  // for (let i = 1; i <= 20; i++) {
+  //   if (details.meals[0][`strIngredient${[i]}`]) {
+  //     ingredientsArray.push({
+  //       measure: details.meals[0][`strMeasure${[i]}`],
+  //       ingredients: details.meals[0][`strIngredient${[i]}`],
+  //     });
+  //   } else {
+  //     break;
+  //   }
+  // }
+  // console.log("ingredientsArray: ", ingredientsArray);
+
+  //________LOOOOOP__________________________________________2______________________
   return (
-    <div className="card">
-      {details.meals && <div className="card-body">
-      <img
-        src={details.meals[0].strMealThumb}
-        className="card-img-top"
-        alt={details.meals[0].strMeal}
-      />
-      <h3 className="card-title">{details.meals[0].strMeal}</h3>
-      <div className="card-text">Area: {details.meals[0].strArea}</div>
-      <h3 className="card-text">Ingredients:</h3>
-      <h3>Instructions:</h3>
-      <div className="card-text">{details.meals[0].strInstructions}</div>
-      
-      <div className="d-flex justify-content-between">
-        <button className="recipeButton" onClick={handleClick}>Back to Main</button>
-        <button className="recipeButton">Infinity</button>
-      </div>
-      </div>}
+    <div className="card" key={details.idMeal}>
+      {details.meals && (
+        <div className="card-body">
+          <img
+            src={details.meals[0].strMealThumb}
+            className="card-img-top"
+            alt={details.meals[0].strMeal}
+          />
+          <h3 className="card-title">{details.meals[0].strMeal}</h3>
+          <div className="card-text">Area: {details.meals[0].strArea}</div>
+          <h3 className="card-text">Ingredients:</h3>
+          {/* <div className="card-text">{ingredientsArray}</div> */}
+          <h3>Instructions:</h3>
+          <div className="card-text">{details.meals[0].strInstructions}</div>
+
+          <div className="d-flex justify-content-between">
+            <button className="recipeButton" onClick={handleClick}>
+              Back to Main
+            </button>
+            <button className="recipeButton">Infinity</button>
+          </div>
+        </div>
+      )}
     </div>
-    // <DetailWrapper>
-    //   {details.meals && (
-    //     <>
-    //       <div>
-    //         <h2>{details.meals[0].strMeal}</h2>
-    //         <h3>{details.meals[0].strArea}</h3>
-    //         <img
-    //           src={details.meals[0].strMealThumb}
-    //           alt={details.meals[0].strMeal}
-    //         />
-    //       </div>
-    //       <Info>
-    //         <ul></ul>
-    //         <div>
-    //           {details.meals[0].strInstructions}
-    //           {/* <h3
-    //           dangerouslySetInnerHTML={{ __html: details.strInstructions }}
-    //         ></h3> */}
-    //         </div>
-    //       </Info>
-    //     </>
-    //   )}
-    // </DetailWrapper>
   );
 };
-
-// const DetailWrapper = styled.div`
-//   margin-top: 10rem;
-//   margin-bottom: 5rem;
-//   display: flex;
-//   h2 {
-//     margin-bottom: 2rem;
-//   }
-//   li {
-//     font-size: 1.2rem;
-//     line-height: 2.5rem;
-//   }
-//   ul {
-//     margin-top: 2rem;
-//   }
-// `;
-// const Info = styled.div`
-//   margin-left: 10rem;
-// `;
