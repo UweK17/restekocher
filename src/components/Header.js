b4fbbd61442b05ecf633f19a8d71c36915880971
X-Ella-X <e.felsinger@web.de>
2022-10-25 11:10:26 +0200

    home-link on header icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,12 @@ import logo from "../img/fridge-5930424_1920.png";
 export const Header = () => {
   return (
     <header className="navbar navbar-light bg-light w-auto p-3 text-center d-flex justify-content-around">
-      <a className="navbar-brand" href="https">
+      <a className="navbar-brand" href="/">
         <img src={logo} style={{ width: 70, height: 80 }} alt="logo" />
       </a>
       <h1>Willkommen beim ResteKocher</h1>
-      
-      <div>
-        
-      </div>
+
+      <div></div>
     </header>
   );
 };
