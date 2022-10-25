9904314d123a64709960d653c89a62bd7543a234
Uwe Kremer <uwekremer1971@gmx.de>
2022-10-25 10:58:27 +0200

    info zentriert

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,15 @@ import logo from "../img/fridge-5930424_1920.png";
 
 export const Header = () => {
   return (
-    <header className="navbar navbar-light bg-light w-auto p-3">
+    <header className="navbar navbar-light bg-light w-auto p-3 text-center d-flex justify-content-around">
       <a className="navbar-brand" href="https">
         <img src={logo} style={{ width: 70, height: 80 }} alt="logo" />
       </a>
-      <h1>ResteKocher</h1>
-      <div></div>
+      <h1>Willkommen beim ResteKocher</h1>
+      
+      <div>
+        
+      </div>
     </header>
   );
 };
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,13 +1,25 @@
 import fridge from "../img/fridge-5930424_1920.png";
+import './info.css';
+
 export const Info = () => {
   return (
     <div>
-      <h1>Willkommen beim ResteKocher!</h1>
-      <p>
-        Lieber ratloser Mensch, wenn du nicht weißt, was du kochen sollst, bist
-        du hier genau richtig.
-      </p>
-      <img src={fridge} style={{ width: 300 }} alt="" />
+      <div className="d-flex">
+        <a href="https://www.youtube.com/watch?v=GYK-NfOo7b4" target="_blank" ><img src={fridge} style={{ width: 300 }} alt="bild" className="beat"/></a>
+        <div className="m-4 p-5">
+          <div>
+            Lieber ratloser Mensch, wenn du nicht weißt, was du kochen sollst, bist
+            du hier genau richtig.
+          </div>
+          <div>Kennst du dieses Gefühl, was essen zu wollen, aber nicht weißt was?</div>
+          <div>Du schaust in den Kühlschrank..........nichts!</div>
+          <div>Du schaust in alle Schubladen..........nichts!</div>
+          <div>Du schaust in alle Regale..............nichts!</div>
+          <div>ABER............................das stimmt nicht!</div>
+          <div>Sag mir eine Zutat, und ich zaubere Dir ein leckeres Essen!</div>
+        </div>
+
+      </div>
     </div>
   );
 };
